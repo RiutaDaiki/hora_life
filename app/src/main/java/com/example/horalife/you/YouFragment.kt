7fc892d88a209a822fa6cf247e78c2bacf1e60a5
RiutaDaiki <daikiriuta0926@gmail.com>
2021-05-28 15:59:05 +0900

    ログイン・ログアウト機能を実装した

--- app/src/main/java/com/example/horalife/you/YouFragment.kt
+++ app/src/main/java/com/example/horalife/you/YouFragment.kt
@@ -1,10 +1,14 @@
 package com.example.horalife.you
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.horalife.R
 import com.example.horalife.databinding.YouFragmentBinding
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.ktx.auth
@@ -14,39 +18,65 @@ class YouFragment : Fragment() {
     private lateinit var binding: YouFragmentBinding
     private val SIGN_IN = 9001
 
+    @SuppressLint("ResourceAsColor")
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         binding = YouFragmentBinding.inflate(layoutInflater, container, false)
         binding.lifecycleOwner = viewLifecycleOwner
         val currentUser = Firebase.auth.currentUser
 
-        if (currentUser != null) {
-            binding.user.text = currentUser.displayName
-            binding.statusText.visibility = View.INVISIBLE
-        } else {
-            binding.user.text = "ログインしてません"
-        }
-
-
         val providers = arrayListOf(
-            AuthUI.IdpConfig.EmailBuilder().build()
+                AuthUI.IdpConfig.EmailBuilder().build()
         )
 
-        binding.statusText.setOnClickListener {
-            startActivityForResult(
+        if (currentUser != null) {
+            //ログインしている状態　
+            binding.user.text = currentUser.displayName
+            binding.statusText.text = "ログアウト"
+            binding.statusText.setTextColor(statusColor("red"))
+            binding.statusText.setOnClickListener {
                 AuthUI.getInstance()
-                    .createSignInIntentBuilder()
-                    .setAvailableProviders(providers)
-                    .build(),
-                SIGN_IN
-            )
+                        .signOut(this.requireContext())
+                        .addOnCompleteListener {
+                            Toast.makeText(this.requireContext(), "ログアウト完了", Toast.LENGTH_SHORT).show()
+                        }
+            }
+        } else {
+            binding.user.text = "ログインしてません"
+            binding.statusText.text = "ログイン・登録"
+            binding.statusText.setTextColor(statusColor("blue"))
+            binding.statusText.setOnClickListener {
+                startActivityForResult(
+                        AuthUI.getInstance()
+                                .createSignInIntentBuilder()
+                                .setAvailableProviders(providers)
+                                .build(),
+                        SIGN_IN
+                )
+            }
         }
-
         return binding.root
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
+        if (requestCode == SIGN_IN) {
+            val user = Firebase.auth.currentUser
+            if (user != null) Toast.makeText(context, "ログイン完了", Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
+    private fun statusColor(color: String): Int {
+        var result = resources.getColor(R.color.black)
+        when (color) {
+            "blue" -> result = resources.getColor(R.color.blue)
+            "red" -> result = resources.getColor(R.color.red)
+        }
+        return result
+    }
 }
